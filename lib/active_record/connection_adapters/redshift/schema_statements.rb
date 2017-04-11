df4a174f185e78eca0568e4feb9ca069393b5983
Jason Radice <jason.radice@affectiva.com>
2017-04-11 15:02:15 -0400

    Update to schema_statements.rb to work with rails 5.1.0.beta

--- lib/active_record/connection_adapters/redshift/schema_statements.rb
+++ lib/active_record/connection_adapters/redshift/schema_statements.rb
@@ -4,11 +4,6 @@ module ActiveRecord
       class SchemaCreation < AbstractAdapter::SchemaCreation
         private
 
-        def visit_ColumnDefinition(o)
-          o.sql_type = type_to_sql(o.type, o.limit, o.precision, o.scale)
-          super
-        end
-
         def add_column_options!(sql, options)
           column = options.fetch(:column) { return super }
           if column.type == :uuid && options[:default] =~ /\(\)/
@@ -372,7 +367,7 @@ module ActiveRecord
         end
 
         # Maps logical Rails types to PostgreSQL-specific data types.
-        def type_to_sql(type, limit = nil, precision = nil, scale = nil)
+        def type_to_sql(type, limit: nil, precision: nil, scale: nil, **)
           case type.to_s
           when 'integer'
             return 'integer' unless limit
